347e2e9c6389cfa03b9d8bfa1f1bf61a65bc48f0
Littledude14647 <Littledude14647@gmail.com>
2025-09-06 08:41:46 -0500

    update main to get book code

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("greetings boots!")
-print("Lets make some actual code now yippieeee!!!!!")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+    
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()
